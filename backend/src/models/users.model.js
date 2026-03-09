f4fdc10288798fb2cbc0f5d7043cf95947c59b9b
dangtmhe182287 <dangtmhe182287@fpt.edu.vn>
2026-03-09 13:35:33 +0700

    Merge branch 'dev' of https://github.com/dangtmhe182287/WDP301_Group6 into dev

--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -1,13 +1,45 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
-    fullName: {type: String},
-    email: {type: String, required: true},
-    password: {type: String, required: true}, 
-    phone: {type: String},
-    role:{type: String, enum:["customer", "admin", "staff"], default:"user"},
-    imgUrl: {type: String},
-    refreshToken:{type: String},
-    membershipId: {type: Number},
-},{timestamps:true})
+    fullName: { type: String },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    phone: { type: String },
+    role: { type: String, enum: ["customer", "admin", "staff"], default: "custome" },
+    imgUrl: { type: String },
+    refreshToken: { type: String },
+    membershipId: { type: Number },
+    membershipType: {
+        type: String,
+        enum: ['normal', 'gold', 'diamond'],
+        default: 'normal'
+    },
+
+    //staff specific profile fields
+    speciality: {
+        type: String,
+        trim: true,
+        validate: {
+            validator(value) {
+                if (this.role != "staff") return true; // if not staff, return
+                return Boolean(value && value.trim().length > 0); //if true, pass validator
+            },
+            message: "Speciality is required for staff accounts",
+        },
+    },
+
+    experienceYears: {
+        type: Number,
+        min: 0,
+        validate: {
+            validator(value) {
+                if (this.role != "staff") return true; // if not staff, return
+                return Number.isInteger(value) && value >= 0; //if true, pass validator
+            },
+            message: "Experience years must be non negative inter for staff accounts",
+        },
+    },
+},
+    { timestamps: true }
+);
 const User = mongoose.model("User", userSchema);
 export default User;
